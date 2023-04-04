0da362cff47e7f566d9242cd0e3c20ab7a5bc5b0
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 02:59:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+const appDiv = document.getElementById('app');
+const swapBtn = document.getElementById('swap');
 
+function swapTheme() {
+if (appDiv.classList.contains('day')) {
+appDiv.classList.remove('day');
+appDiv.classList.add('night');
+swapBtn.classList.remove('button_day');
+swapBtn.classList.add('button_night');
+} else {
+appDiv.classList.remove('night');
+appDiv.classList.add('day');
+swapBtn.classList.remove('button_night');
+swapBtn.classList.add('button_day');
+}
+}
+
+swapBtn.addEventListener('click', swapTheme);
