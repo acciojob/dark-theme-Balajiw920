0550e3d5645aa18bf67fdfc3b4dd18520f339fba
Balajiw920 <wbalaji2021@gmail.com>
2023-05-28 16:45:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 //your code here
-const appDiv = document.getElementById('app');
-const swapBtn = document.getElementById('swap');
+// Get the necessary elements from the DOM
+const appElement = document.getElementById("app");
+const pElement = document.getElementById("ptag");
+const swapButton = document.querySelector("button");
 
+// Add event listener to the button
+swapButton.addEventListener("click", swapTheme);
+
+// Function to swap the theme
 function swapTheme() {
-if (appDiv.classList.contains('day')) {
-appDiv.classList.remove('day');
-appDiv.classList.add('night');
-swapBtn.classList.remove('button_day');
-swapBtn.classList.add('button_night');
-} else {
-appDiv.classList.remove('night');
-appDiv.classList.add('day');
-swapBtn.classList.remove('button_night');
-swapBtn.classList.add('button_day');
-}
-}
+  // Toggle the 'night' class on the appElement
+  appElement.classList.toggle("night");
 
-swapBtn.addEventListener('click', swapTheme);
+  // Toggle the 'button_night' class on the swapButton
+  swapButton.classList.toggle("button_night");
+
+  // Update the text content of the pElement based on the theme
+  if (appElement.classList.contains("night")) {
+    pElement.textContent = "I am an AccioJob student, ready to become an awesome full stack developer in the dark!";
+  } else {
+    pElement.textContent = "I am an AccioJob student, ready to become an awesome full stack developer.";
+  }
+}
